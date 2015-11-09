94bee1b597b7564d5a499fa8ec0c99e086b1c6cb
jdb12@rice.edu <jdb12@rice.edu>
2015-11-09 18:13:12 -0500

    added scripts and submit file for baseQA

---
+++ scripts/baseQA.C
@@ -0,0 +1,77 @@
+#include "iostream.h"
+
+class     StChain;
+class     St_db_Maker;
+
+
+StChain  *chain=0;
+
+void baseQA( const Char_t *fileList = "small.lis",
+					const Char_t *name = ".root",
+					int maxEvents = 50000,
+					int maxFiles = 10 )
+{
+  	
+
+  	// Load libraries
+	gROOT->Macro("loadMuDst.C");
+	gROOT->LoadMacro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
+	loadSharedLibraries();
+	
+	gSystem ->Load("StChain");
+    gSystem ->Load("StMiniMcEvent");
+	gSystem->Load("StRefMultCorr");
+	gSystem->Load("StRooBarb");
+	gSystem->Load("StRcpPicoMaker");
+	
+	// Create Chain
+	chain  = new StChain("StChain");
+
+	// create the StMuDstMaker
+	StMuDstMaker *muDstMaker = new StMuDstMaker(  0, 0, "", fileList, "MuDst.root", maxFiles );
+
+	StRcpQAMaker *rcpQA = new StRcpQAMaker("rcpQA", ("qa_" + string(name) ).c_str() );
+	// StRcpPicoMaker *rcpPico = new StRcpPicoMaker("rcpPico", ("tuple_" + string(ntuplename)).c_str() ) ;
+
+	// Initialize chain
+	Int_t iInit = chain->Init();
+	if (iInit) 
+		chain->Fatal(iInit,"on init");
+	
+	chain->PrintInfo();
+
+
+	bool good = true;
+	int iEvent = 0;
+	while ( good ){
+
+		if ( iEvent >= maxEvents )
+			break;
+
+		chain->Clear();
+		int istat = 0;
+		istat = chain->Make(iEvent);
+		if (istat == 2) {
+			cout << "Last  event processed. Status = " << istat << endl;
+		}
+		if (istat == 3) {
+			cout << "Error event processed. Status = " << istat << endl;
+		}
+
+		if ( iEvent % 1000 == 0 ) {
+			cout << " Event " << iEvent << " Complete" << endl;
+		}
+
+		iEvent++;
+	}
+
+	// Clean up
+	if (iEvent >= 1) {
+		chain->Finish();
+		delete chain;
+	}
+
+}
+
+
+
